5ad7834445d29718aeeb3a919b5ffbb95d4f3d99
Utkarsh Gupta <utkarshg789@gmail.com>
2023-07-13 20:34:20 +0530

    first commit

---
+++ src/components/ContactList.js
@@ -0,0 +1,41 @@
+import React from "react";
+import {Link} from "react-router-dom"
+import ContactCard from "./ContactCard";
+
+const ContactList = (props) => {
+  console.log(props);
+
+  const deleteConactHandler = (id) => {
+    props.getContactId(id);
+  };
+ 
+  const renderContactList = props.contacts.map((contact) => {
+    return (
+      <ContactCard
+        contact={contact}
+        clickHander={deleteConactHandler}
+        key={contact.id}
+      />
+    );
+  });
+  return (
+    <div className='main'>
+        <h2>Contact List 
+            <Link to="/add">
+                <button className='ui primary button right floated'>Add Contact</button>
+            </Link>
+        </h2>
+        {/* <div className='ui search'>
+            <div className='ui icon input'>
+                <input ref={inputEl} type="text" placeholder='Search Contact' className='prompt' value={props.term} onChange={getSearchTerm}/>
+                <i className='search icon'/>
+            </div>
+        </div> */}
+        <div className='ui celled list'>
+            {renderContactList.length >0 ? renderContactList:"No Contacts available"}
+        </div>
+    </div>
+)
+};
+
+export default ContactList;
